16e4d86e10ecf4ba89deefc83344b77b4ffa46a0
CMederic <mederic.cuny9@etu.univ-lorraine.fr>
2026-01-16 09:34:04 +0100

    fix: corriger la définition de CreateOrderResponse pour utiliser un type

--- frontend/src/api/orders.ts
+++ frontend/src/api/orders.ts
@@ -11,8 +11,7 @@ export interface Order {
   created_at: string;
 }
 
-//  @typescript-eslint/no-empty-object-type
-interface CreateOrderResponse extends Order {}
+type CreateOrderResponse = Order;
 
 interface OrderError {
   error: string;
